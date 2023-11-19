83ccc73670d4f1e055b14356824db144a4fd9b6a
Mawyn Nhek <mawyn.nhek@gmail.com>
2023-11-19 14:21:42 +0100

    feat: added docker support

--- main.go
+++ main.go
@@ -4,16 +4,21 @@ import (
 	"os"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/joho/godotenv"
 )
 
 func main() {
-	port := os.Getenv("SERVER_PORT")
+	if err := godotenv.Load(".env"); err != nil {
+		panic("No .env file found")
+	}
+
+	server_port := os.Getenv("SERVER_PORT")
 
 	app := fiber.New()
 
 	app.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("Port: " + port)
+		return c.SendString("Port: " + server_port)
 	})
 
-	app.Listen(":8000")
+	app.Listen(":" + server_port)
 }
